25f3815acb2c2a01a8afb05bf351e8213c89005e
Andy <ap3879@drexel.edu>
2024-08-04 00:01:51 -0400

    changed firebase config

--- firebase.js
+++ firebase.js
@@ -1,6 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
-import { firebaseConfig } from "./firebase_config.mjs"
+import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId } from "./firebase_config.mjs"
+
+const firebaseConfig = {
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId
+  };
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
